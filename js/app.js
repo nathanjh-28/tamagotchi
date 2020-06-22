78412d40ea07eee66dbaafd06d903d28993453e9
nathanjh-28 <nathan@nathanjharris.com>
2020-06-22 17:37:07 -0600

    started JS, setUp function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log("Hello Tamagotchi Project");
+
+
+//----- App State ----- //
+
+let name = null;
+let age = 0;
+let hunger = 0;
+let sleepyness = 0;
+let happiness = 0;
+let tColor = null;
+
+// ----- Cached DOM Elements ----- //
+
+
+
+
+// ----- Event Listeners ----- //
+
+
+
+// ----- Functions ----- // 
+
+function setUp (){
+    name = prompt("Enter the name of your pet");
+    tColor = prompt("Select one of the following colors: Pink, Blue, Purple, Green, White");
+}
+
+setUp();
