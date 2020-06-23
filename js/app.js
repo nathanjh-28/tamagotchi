b126461bc86b3b6ddae69a9e1bd86bf85891a16e
nathanjh-28 <nathan@nathanjharris.com>
2020-06-22 18:11:44 -0600

    added event listeners and tested buttons

--- js/app.js
+++ js/app.js
@@ -2,27 +2,51 @@
 
 //----- App State ----- //
 
+//name of the pet
 let name = null;
+// age of the pet
 let age = 0;
+//Metrics
 let hunger = 0;
 let sleepyness = 0;
 let happiness = 0;
+// color of the tamagotchi
 let tColor = null;
 
 // ----- Cached DOM Elements ----- //
 
-
+const feedMe = document.getElementById('feed');
+const playMe = document.getElementById('play-btn');
+const sleepMe = document.getElementById('lites-out-btn'); 
 
 
 // ----- Event Listeners ----- //
 
-
+function addEventListeners (){
+    feedMe.addEventListener('click', handleFeed);
+    playMe.addEventListener('click', handlePlay);
+    sleepMe.addEventListener('click', handleSleep);
+}
 
 // ----- Functions ----- // 
 
 function setUp (){
     name = prompt("Enter the name of your pet");
     tColor = prompt("Select one of the following colors: Pink, Blue, Purple, Green, White");
+    document.getElementById('name').innerText = `Name: ${name}`;
+    document.getElementById('console').style.backgroundColor = tColor;
+    addEventListeners();
+}
+setUp();
+
+function handleFeed (){
+    console.log('feed button has been clicked');
 }
 
-setUp();
+function handlePlay(){
+    console.log('play button has been clicked');
+}
+
+function handleSleep(){
+    console.log('sleep button has been clicked');
+}
